d36c0c0a77baa8c2bddde7f332de44de7b33436e
Mischa Lisovyi <mischa.lisovyi@mgb.ch>
2022-04-30 13:30:28 +0200

    enable aggregation over the starting period

--- save_life_in_ua/plot.py
+++ save_life_in_ua/plot.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import matplotlib.pyplot as plt
 import pandas as pd
 from wordcloud import WordCloud
@@ -12,10 +13,26 @@ def add_war_start_line() -> None:
     plt.axvline(x=dt.date(2022, 2, 24), ls="--", c="k", label="Russia's invasion")
 
 
-def plot_daily_inout(s_in: pd.Series, s_out: pd.Series) -> None:
+def preprocess_before_plotting(
+    s: pd.Series, date_start: Optional[str] = None
+) -> pd.Series:
+    s_out = s.copy()
+    if date_start:
+        date = pd.to_datetime(date_start, format="%Y-%m-%d")
+        mask = s_out.index <= pd.to_datetime(date_start, format="%Y-%m-%d")
+        s_agg = pd.Series([s[mask].sum()], index=[date])
+        s_out = pd.concat([s_agg, s_out[~mask]], axis=0)
+    return s_out
+
+
+def plot_daily_inout(
+    s_in: pd.Series, s_out: pd.Series, date_start: Optional[str] = None
+) -> None:
     plt.figure(figsize=(12, 5))
-    s_in.plot.line("-", marker=".", label="Income")
-    s_out.plot.line("-", marker=".", label="Expenses")
+    _s_in = preprocess_before_plotting(s_in, date_start)
+    _s_in.plot.line("-", marker=".", label="Income")
+    _s_out = preprocess_before_plotting(s_out, date_start)
+    _s_out.plot.line("-", marker=".", label="Expenses")
     plt.xlabel("Date")
     plt.ylabel("Donations/Expenses per day, millions UAH")
     add_y_grid_lines()
@@ -24,14 +41,18 @@ def plot_daily_inout(s_in: pd.Series, s_out: pd.Series) -> None:
     plt.tight_layout()
 
 
-def plot_cum_daily_inout(s_in: pd.Series, s_out: pd.Series) -> None:
+def plot_cum_daily_inout(
+    s_in: pd.Series, s_out: pd.Series, date_start: Optional[str] = None
+) -> None:
     _, axs = plt.subplots(
         2, 1, figsize=(12, 7), sharex="col", gridspec_kw={"height_ratios": [2.5, 1]}
     )
     # top figure with the cumulative distribution of donations and expenses
     plt.sca(axs[0])
-    s_in.plot.line("-", marker=".", label="Income")
-    s_out.plot.line("-", marker=".", label="Expenses")
+    _s_in = preprocess_before_plotting(s_in, date_start).cumsum()
+    _s_in.plot.line("-", marker=".", label="Income")
+    _s_out = preprocess_before_plotting(s_out, date_start).cumsum()
+    _s_out.plot.line("-", marker=".", label="Expenses")
     plt.xlabel("Date")
     plt.ylabel("Cumulative each day, millions UAH")
     add_y_grid_lines()
@@ -39,7 +60,7 @@ def plot_cum_daily_inout(s_in: pd.Series, s_out: pd.Series) -> None:
     plt.legend()
     # bottom figure with the savings
     plt.sca(axs[1])
-    df_inout = pd.concat([s_in.rename("in"), s_out.rename("out")], axis=1)
+    df_inout = pd.concat([_s_in.rename("in"), _s_out.rename("out")], axis=1)
     max_date = df_inout.idxmax().min()
     df_inout = df_inout[:max_date].fillna(method="ffill")
     s_diff = df_inout["in"] - df_inout["out"]
