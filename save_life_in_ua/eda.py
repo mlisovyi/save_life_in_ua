948f0155d3605d680cb29fdf3d2a07e1ca0f8c8b
Mischa Lisovyi <mischa.lisovyi@mgb.ch>
2022-04-30 13:32:20 +0200

    improve top-N table + agg start of timeseries

--- save_life_in_ua/eda.py
+++ save_life_in_ua/eda.py
@@ -18,15 +18,21 @@ df_out = sliu.process_date(df_out)
 
 # %%
 pd.set_option("max_colwidth", 200)
-df_in[df_in["date"] >= dt.date(2022, 1, 1)].nlargest(20, columns="amount")
+col_m_uah = "amount, millions UAH"
+(
+    df_in[df_in["date"] >= pd.to_datetime("2022-01-01")]
+    .nlargest(20, columns="amount")
+    .reset_index(drop=True)
+    .assign(**{col_m_uah: (lambda x: x["amount"] / 1e6)})[["date", col_m_uah, "source"]]
+)
 
 # %%
 # aggregate and transform values into millions
 df_in_date = sliu.get_daily_total(df_in)
 df_out_date = sliu.get_daily_total(df_out)
 # %%
-sliu.plot_daily_inout(df_in_date, df_out_date)
-sliu.plot_cum_daily_inout(df_in_date.cumsum(), df_out_date.cumsum())
+sliu.plot_daily_inout(df_in_date, df_out_date, date_start="2022-02-01")
+sliu.plot_cum_daily_inout(df_in_date, df_out_date, date_start="2022-02-01")
 # %%
 s_final = sliu.process_type_of_expenses(df_out["type"])
 
