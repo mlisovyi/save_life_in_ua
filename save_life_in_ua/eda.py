c4a67100709ae87446d6ac219c87339356f20b82
Mischa <mischa.lisovyi@mgb.ch>
2022-05-17 07:35:29 +0200

    add comments to the main script

--- save_life_in_ua/eda.py
+++ save_life_in_ua/eda.py
@@ -1,3 +1,5 @@
+"""The main script to read in data, process and dump the markdown report.
+"""
 # %%
 from pathlib import Path
 
@@ -9,6 +11,7 @@ import save_life_in_ua as sliu
 if __name__ == "__main__":
     # %%
     file_data = Path("data/militarnyy_2022.xlsb")
+    # this will download the data
     sliu.download_data(file_data)
 
     # %%
@@ -18,7 +21,7 @@ if __name__ == "__main__":
     # Largest donations
     pd.set_option("max_colwidth", 200)
     df_top_n = sliu.get_top_N_donations(df_in)
-    df_top_n
+    # df_top_n
 
     # %%
     # aggregate and transform values into millions
@@ -28,6 +31,7 @@ if __name__ == "__main__":
     # %%
     dir_out = Path("docs/figs/")
     # plot daily sums
+    # generate plots and dump plotsthat will be picked up by the Markdown
     sliu.plot_daily_inout(
         df_in_date, df_out_date, date_start="2022-02-01", fout=dir_out / "daily.png"
     )
@@ -44,6 +48,7 @@ if __name__ == "__main__":
     sliu.plot_word_cloud_expenses(s_final)
 
     # %%
+    # generate the final markdown report
     sliu.generate_markdown(df_top_n)
 
 # %%
